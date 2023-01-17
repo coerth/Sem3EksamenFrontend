501184d612712a5992eb4c159eb49f523db9ed97
Morten Bendeke <mbendeke@hotmail.com>
2023-01-17 17:23:41 +0100

    deploy process klaret

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,7 +11,7 @@ function App() {
 
   return (
     <div className="App">
-      <BrowserRouter>
+      <BrowserRouter basename='frontend'>
       <Header setLoggedIn={setLoggedIn} loggedIn={loggedIn}/>
 
       
